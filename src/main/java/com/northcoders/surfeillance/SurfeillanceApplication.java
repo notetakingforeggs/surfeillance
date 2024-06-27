226ec216ce4bb44f4180404a2fd7b090149eaa80
ste <mrbarrow@hotmail.co.uk>
2024-06-27 13:58:51 +0100

    initial commit

---
+++ src/main/java/com/northcoders/surfeillance/SurfeillanceApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.surfeillance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfeillanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfeillanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/surfeillance/SurfeillanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.surfeillance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfeillanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
