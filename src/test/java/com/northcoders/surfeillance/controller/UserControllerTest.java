2dc3bd89cdae715506d136c27f4fc559e4ca17db
ste <mrbarrow@hotmail.co.uk>
2024-06-28 16:10:31 +0100

    added test classes for controllers

---
+++ src/test/java/com/northcoders/surfeillance/controller/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.northcoders.surfeillance.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void getTripsByUser() {
+    }
+
+    @Test
+    void getTripById() {
+    }
+
+    @Test
+    void addTrip() {
+    }
+
+    @Test
+    void updateTrip() {
+    }
+}
