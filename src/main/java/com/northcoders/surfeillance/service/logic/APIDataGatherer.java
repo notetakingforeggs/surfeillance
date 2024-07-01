84a2bc3d96ede6ddbfd8736c8241c5dc979ab123
ste <mrbarrow@hotmail.co.uk>
2024-07-01 14:42:19 +0100

    apigatherer weather/marine + tests

---
+++ src/main/java/com/northcoders/surfeillance/service/logic/APIDataGatherer.java
@@ -0,0 +1,19 @@
+package com.northcoders.surfeillance.service.logic;
+
+import com.northcoders.surfeillance.model.Coordinate;
+import com.northcoders.surfeillance.service.apis.waveAPI.CurrentMarineData;
+import com.northcoders.surfeillance.service.apis.waveAPI.waveDAO.MarineConditionsDAO;
+import com.northcoders.surfeillance.service.apis.weatherAPI.CurrentWeatherData;
+import com.northcoders.surfeillance.service.apis.weatherAPI.weatherDAO.WindConditionsDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class APIDataGatherer {
+
+    public CurrentWeatherData fetchWeather(Coordinate coordinate) {
+      return WindConditionsDAO.getWindCondition(coordinate.getLatitude(), coordinate.getLongitude());
+    };
+
+    public CurrentMarineData fetchMarineData(Coordinate coordinate) {
+        return MarineConditionsDAO.getMarineCondition(coordinate.getLatitude(), coordinate.getLongitude());
+    }
+}
