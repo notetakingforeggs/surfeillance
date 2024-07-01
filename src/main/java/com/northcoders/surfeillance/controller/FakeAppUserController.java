577eeba5df3cff1e9af060bae4f50e3fee4105f9
ludic <notetakingforeggs@gmail.com>
2024-07-01 11:02:40 +0100

    Feat: fake users controller written, all three dummy endpoints tested and working via postman

--- src/main/java/com/northcoders/surfeillance/controller/FakeAppUserController.java
+++ src/main/java/com/northcoders/surfeillance/controller/FakeAppUserController.java
@@ -1,20 +1,26 @@
 package com.northcoders.surfeillance.controller;
 
 import com.northcoders.surfeillance.model.FakeAppUser;
+import com.northcoders.surfeillance.model.FakeLocation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("fakeusers")
 public class FakeAppUserController {
 
-    @GetMapping
-    public ResponseEntity<List<FakeAppUser>> getFakeUser(){
+      List<Integer> faveLocationIds = new ArrayList<Integer>(List.of(1,2,3));
 
+
+    @GetMapping
+    public ResponseEntity<FakeAppUser> getFakeUser(){
+    return new ResponseEntity<>(new FakeAppUser(1, "keanu reeves", "hawaii",faveLocationIds, 5 ), HttpStatus.OK);
     }
 
 }
