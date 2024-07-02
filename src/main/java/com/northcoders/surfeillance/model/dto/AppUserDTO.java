4f21e2ff8995a036868dd58e6362726e389d78ec
ste <mrbarrow@hotmail.co.uk>
2024-07-02 16:39:24 +0100

    added AppUserDTO attributes

--- src/main/java/com/northcoders/surfeillance/model/dto/AppUserDTO.java
+++ src/main/java/com/northcoders/surfeillance/model/dto/AppUserDTO.java
@@ -1,4 +1,15 @@
 package com.northcoders.surfeillance.model.dto;
 
+import com.northcoders.surfeillance.model.SkillLevel;
+import lombok.Data;
+
+@Data
 public class AppUserDTO {
+
+    private long id;
+    private String username;
+    private String profileText;
+    private String lcoation;
+    private SkillLevel skillLevel;
+
 }
