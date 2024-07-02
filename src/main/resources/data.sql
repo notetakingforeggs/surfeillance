04040cdcd36e26f6750d439634d8c4aa2ebb34d3
ste <mrbarrow@hotmail.co.uk>
2024-07-02 12:32:15 +0100

    fix: data load issue

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,21 @@
-INSERT INTO spot
-(spot_id, location_name, latitude, longitude, tide_station_id)
+INSERT INTO spot (spot_id, location_name, latitude, longitude, tide_station_id)
 VALUES
 (1, 'Castle Haven Bay', -4.1925652, 54.8083162, '0324'),
 (2, 'Kirkandrews Bay', -4.1885101, 54.8072633, '0324'),
 (3, 'Unknown', -4.259116, 54.837657, '0324'),
 (4, 'Ross Bay', -4.0919856, 54.7785707, '0324'),
-(5, 'Ringdoo Sands', -4.8105347, 54.8492277, '0324');
+(5, 'Ringdoo Sands', -4.8105347, 54.8492277, '0324')
+ON CONFLICT (spot_id) DO NOTHING;
 
-
-INSERT INTO app_user
-(id, user_name, location, profile_text, skill_level)
+INSERT INTO app_user (id, user_name, location, profile_text, skill_level)
 VALUES
 (1, 'Ste', 'UK', 'Not much of a surfer', 0),
 (2, 'Tom', 'UK', 'Not much of a surfer', 0),
 (3, 'Francis', 'UK', 'Not much of a surfer', 0),
-(4, 'Jonah', 'UK', 'Quite the surfer', 2);
+(4, 'Jonah', 'UK', 'Quite the surfer', 2)
+ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO trip
-(trip_id, info_rating, surf_rating, spot_id, user_id)
+INSERT INTO trip (trip_id, info_rating, surf_rating, spot_id, user_id)
 VALUES
 (1, 2, 3, 3, 1),
 (2, 4, 5, 1, 2),
@@ -43,4 +41,5 @@ VALUES
 (22, 2, 1, 2, 4),
 (23, 3, 2, 1, 1),
 (24, 1, 1, 5, 2),
-(25, 5, 3, 4, 3);
+(25, 5, 3, 4, 3)
+ON CONFLICT (trip_id) DO NOTHING;
