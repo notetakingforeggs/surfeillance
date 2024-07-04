355feb4a495dbb502320a505677f763e677a6dfb
ste <mrbarrow@hotmail.co.uk>
2024-07-04 10:27:58 +0100

    feat: TripDTO

--- src/main/java/com/northcoders/surfeillance/model/dto/TripDTO.java
+++ src/main/java/com/northcoders/surfeillance/model/dto/TripDTO.java
@@ -1,19 +1,43 @@
 package com.northcoders.surfeillance.model.dto;
 
-import com.northcoders.surfeillance.service.apis.tidalapi.TidalEventDAO;
+import com.northcoders.surfeillance.model.Spot;
+import com.northcoders.surfeillance.model.Trip;
+import lombok.Data;
 
 import java.time.LocalDate;
 
-/*
-This TripDTO can be used to
-    return details of a trip while a user is looking at their list of trips
-    it will contain a
-        Trip
-        Spot
-        AppUser
- */
-
+@Data
 public class TripDTO {
+
+    private long tripId;
+    private AppUserDTO appUserDTO;
+    private Spot spot;
+    private Integer surfRating;
+    private Integer infoRating;
+    LocalDate date;
+    private Double waveHeight;
+    private String waveDirection;
+    private Double wavePeriod;
+    private Double windSpeed;
+    private String windDirection;
+    private Integer gusts;
+    private Double tideHeight;
+
+    public TripDTO(Trip trip) {
+        this.tripId = trip.getTripId();
+        this.appUserDTO = new AppUserDTO(trip.getUser());
+        this.spot = trip.getSpot();
+        this.surfRating = trip.getSurfRating();
+        this.infoRating = trip.getInfoRating();
+        this.date = trip.getDate();
+        this.waveHeight = trip.getWaveHeight();
+        this.waveDirection = this.getWaveDirection();
+        this.wavePeriod = trip.getWavePeriod();
+        this.windSpeed = trip.getWindSpeed();
+        this.windDirection = trip.getWindDirection();
+        this.gusts = trip.getGusts();
+        this.tideHeight = trip.getTideHeight();
+    }
 
 
 
