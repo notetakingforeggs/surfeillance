0929abcc27d0f7ad966942e2c40ec32f5a7a68f8
ste <mrbarrow@hotmail.co.uk>
2024-07-08 15:24:44 +0100

    refactor: flattened ConditionsDTO shape

--- src/main/java/com/northcoders/surfeillance/model/dto/ConditionsDTO.java
+++ src/main/java/com/northcoders/surfeillance/model/dto/ConditionsDTO.java
@@ -1,11 +1,5 @@
 package com.northcoders.surfeillance.model.dto;
 
-
-/*
-The parent DTO object that wil be used to provide data to the frontend.
-Will be built in the service layer from a combo of database and 3P-API calls
- */
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.northcoders.surfeillance.model.Spot;
 import com.northcoders.surfeillance.service.apis.tidalapi.daomodel.TidalEvent;
@@ -17,19 +11,35 @@ import lombok.Data;
 @Data
 public class ConditionsDTO {
 
-    @JsonProperty("spot")
-    private Spot spot;
-    @JsonProperty("currentMarineData")
-    private CurrentMarineData currentMarineData;
-    @JsonProperty("currentWeatherData")
-    private CurrentWeatherData currentWeatherData;
-    @JsonProperty("tidalEvent")
-    private TidalEvent tidalEventDTO;
+    String name;
+    String details;
+    String requestTime;
+    Double latitude;
+    Double longitude;
+    String timezone;
+    Long waveHeight;
+    Long waveDirection;
+    Long wavePeriod;
+    Double windSpeed;
+    Double windDirection;
+    Double gusts;
+    Double highTideHeight;
+    Double lowTideHeight;
 
     public ConditionsDTO(Spot spot, CurrentMarineData currentMarineData, CurrentWeatherData currentWeatherData, TidalEvent tidalEventDTO) {
-        this.spot = spot;
-        this.currentMarineData = currentMarineData;
-        this.currentWeatherData = currentWeatherData;
-        this.tidalEventDTO = tidalEventDTO;
+        this.name = spot.getLocationName();
+        this.details = spot.getLocationName() + " is a beautiful place to go for a surf";
+        this.requestTime = currentMarineData.current().time();
+        this.latitude = spot.getCoordinate().getLatitude();
+        this.longitude = spot.getCoordinate().getLongitude();
+        this.timezone = currentMarineData.timezone();
+        this.waveHeight = currentMarineData.current().wave_height();
+        this.waveDirection = currentMarineData.current().wave_direction();
+        this.wavePeriod = currentMarineData.current().wave_period();
+        this.windSpeed = currentWeatherData.current().wind_speed_10m();
+        this.windDirection = currentWeatherData.current().wind_direction_10m();
+        this.gusts = currentWeatherData.current().wind_gusts_10m();
+        this.highTideHeight = tidalEventDTO.getHeight();
+        this.lowTideHeight = tidalEventDTO.getHeight();
     }
 }
