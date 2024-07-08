cb5981c6de777c6a3c8e550680787296bcfca0b5
FrancisAdewale <71708756+FrancisAdewale@users.noreply.github.com>
2024-07-08 15:11:22 +0100

    Oauth2 (#18)
    * update: oauth refactor
    
    * update: help
    
    * update: help 4
    
    * update: secured endpoints
    
    ---------
    
    Co-authored-by: yanader <mrbarrow@hotmail.co.uk>

---
+++ src/main/java/com/northcoders/surfeillance/config/FirebaseConfig.java
@@ -0,0 +1,39 @@
+package com.northcoders.surfeillance.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
+
+    @Value("${firebase.config.path}")
+    private String configPath;
+
+
+    @PostConstruct
+    public void init() {
+        try {
+            FileInputStream serviceAccount =  new FileInputStream("src/main/resources/serviceAccountKey.json");
+//            ClassPathResource resource = new ClassPathResource(configPath);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+            logger.info("App name: {}", FirebaseApp.getInstance().getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/northcoders/surfeillance/controller/OAuthController.java
+++ src/main/java/com/northcoders/surfeillance/controller/OAuthController.java
@@ -1,35 +1,48 @@
 package com.northcoders.surfeillance.controller;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.northcoders.surfeillance.model.dto.MessageDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("oauth")
 public class OAuthController {
 
-    private final JwtDecoder jwtDecoder;
+   // private final JwtDecoder jwtDecoder;
+    private static final Logger logger = LoggerFactory.getLogger(OAuthController.class);
 
-    public OAuthController(JwtDecoder jwtDecoder) {
-        this.jwtDecoder = jwtDecoder;
-    }
+//    public OAuthController(JwtDecoder jwtDecoder) {
+//        this.jwtDecoder = jwtDecoder;
+//    }
 
+//
+//    @GetMapping("/check")
+//    public ResponseEntity<String> checkToken(@RequestParam String token) {
+//        try {
+//            Jwt decodedToken = jwtDecoder.decode(token);
+//            // Validate the token and perform any necessary checks
+//            return ResponseEntity.ok("Token is valid: " + decodedToken.getSubject());
+//        } catch (JwtException e) {
+//            return ResponseEntity.status(401).body("Invalid token");
+//        }
+//    }
 
-    @GetMapping("/check")
-    public ResponseEntity<String> checkToken(@RequestParam String token) {
-        try {
-            Jwt decodedToken = jwtDecoder.decode(token);
-            // Validate the token and perform any necessary checks
-            return ResponseEntity.ok("Token is valid: " + decodedToken.getSubject());
-        } catch (JwtException e) {
-            return ResponseEntity.status(401).body("Invalid token");
-        }
+    @GetMapping("/hello")
+    public ResponseEntity<MessageDto> hello() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(new MessageDto("Hello " + username + "from spring boot"));
     }
 
 
